343ead3611d158642424401e9eadd57ff0ef8e60
hugofsd <hugo619@outlook.com>
2021-11-14 03:48:36 -0300

    Visual da page não encontrada

--- src/app/core/page-nao-encontrada.component.ts
+++ src/app/core/page-nao-encontrada.component.ts
@@ -3,8 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-nao-encontrada',
   template: `
-  <div class="container">
+  <div class="containerVazio">
     <h1 class="text-center">Página não encontrada</h1>
+
+    <img src="../../assets/pageNãoEncontrada.svg" 
+     width="300px" 
+    layout-align="center" >
     </div>
   `,
   styles: [
